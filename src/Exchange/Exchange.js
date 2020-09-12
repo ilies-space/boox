a781bb63a12e7a4fec897d44c5d3554a3672ba05
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2020-09-12 04:26:26 +0100

    updating screens states

--- src/Exchange/Exchange.js
+++ src/Exchange/Exchange.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Colors from '../Colors';
 
-export const Exchange = () => {
+export const Exchange = ({navigation}) => {
   return (
-    <View>
-      <Text>This is Exchange</Text>
+    <View style={styles.mainView}>
+      <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+        <MaterialCommunityIcons
+          name="stack-exchange"
+          color={Colors.MainColor}
+          size={84}
+        />
+
+        <Text style={{color: 'gray', fontSize: 20}}>
+          هذه الخاصية غير متوفرة حاليا
+        </Text>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+          <Text
+            style={{
+              marginTop: 55,
+              backgroundColor: 'gray',
+              padding: 5,
+              fontSize: 16,
+              borderRadius: 15,
+            }}>
+            العودة الى الرئيسية
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainView: {
+    backgroundColor: 'black',
+    flex: 1,
+  },
+});
